3ad8b33920f3df8d32c6896d8aff10589dcb908e
ViggoTrustAnchor <viggo@trustanchorgroup.com>
2025-09-27 17:29:15 +0200

    smarter tracker so sorting is not needed so offen but might make reads very slightly slower

--- VanadiumGame/src/Platformer/ComponentStore.cpp
+++ VanadiumGame/src/Platformer/ComponentStore.cpp
@@ -1,6 +1,55 @@
 #include "ComponentStore.h"
 
-void ComponentTracker::Flush()
+void ComponentTracker::InsertLookup(unsigned int id, unsigned int componentIndex)
+{
+	if (EmptySlots.size() == 0)
+	{
+		Lookups.emplace_back(id, componentIndex);
+		return;
+	}
+
+	Lookups[EmptySlots.back()] = ComponentLookup(id, componentIndex);
+	EmptySlots.pop_back();
+
+}
+
+unsigned int ComponentTracker::FindLookupIndex(unsigned int id)
+{
+
+	int left = 0;
+	int right = Lookups.size() - 1;
+	int foundId;
+	int maxIterations = log2(Lookups.size()) + 2;
+
+	int i = 0;
+	while ((foundId = Lookups[(left + right)/2].Id) != id && i < maxIterations)
+	{
+		if (foundId <= id)
+			left = ceil((left + right) / 2.0f);
+		else
+			right = floor((left + right) / 2.0f);
+
+		i++;
+	}
+
+	if (i == maxIterations)
+		return -1;
+
+	return (left + right) / 2;
+}
+
+unsigned int ComponentTracker::MarkRemoved(unsigned int id)
+{
+	unsigned int lookupIndex = FindLookupIndex(id);
+
+	if (lookupIndex == -1)
+		throw "dasdas";
+
+	EmptySlots.push_back(lookupIndex);
+	return Lookups[lookupIndex].ComponentIndex;
+}
+
+void ComponentTracker::Sort()
 {
 	int layerSize = 1;
 
@@ -31,4 +80,26 @@ void ComponentTracker::Flush()
 		}
 		layerSize *= 2;
 	}
+}
+
+void ComponentTracker::Flush()
+{
+	for (int i = 0; i < EmptySlots.size(); i++)
+	{
+		std::swap(Lookups[EmptySlots[i]], Lookups[Lookups.size() - 1 - i]);
+	}
+	Lookups.resize(Lookups.size() - EmptySlots.size());
+	EmptySlots.clear();
+
+	Sort();
+}
+
+void ComponentTracker::UpdateComponentIndex(unsigned int id, unsigned int newComponentIndex)
+{
+	unsigned int lookupIndex = FindLookupIndex(id);
+
+	if (lookupIndex == -1)
+		throw "dasdas";
+
+	Lookups[lookupIndex].ComponentIndex = newComponentIndex;
 }
--- VanadiumGame/src/Platformer/ComponentStore.h
+++ VanadiumGame/src/Platformer/ComponentStore.h
@@ -8,18 +8,22 @@
 struct ComponentLookup
 {
 	ComponentLookup() = default;
-	ComponentLookup(unsigned int id, unsigned int index)
-		: Id(id), Index(index) {
+	ComponentLookup(unsigned int id, unsigned int componentIndex)
+		: Id(id), ComponentIndex(componentIndex) {
 	};
 
 	unsigned int Id = 0;
-	unsigned int Index = 0;
+	unsigned int ComponentIndex = 0;
 };
 
 struct ComponentTracker
 {
-	unsigned int FindLookupIndex();
+	void InsertLookup(unsigned int id, unsigned int componentIndex);
+	unsigned int FindLookupIndex(unsigned int id);
+	unsigned int MarkRemoved(unsigned int id);
+	void Sort();
 	void Flush();
+	void UpdateComponentIndex(unsigned int id, unsigned int newComponentIndex);
 
 	std::vector<ComponentLookup> Lookups;
 	std::vector<unsigned int> EmptySlots;
@@ -49,14 +53,11 @@ public:
 	ComponentRef<TComponent> CreateInstance()
 	{
 		m_components.PushBack(TComponent());
-		m_lookups.PushBack(ComponentLookup(m_components.Last().GetId(), m_components.Size() - 1));
+		m_tracker.InsertLookup(m_components.Last().GetId(), m_components.Size() - 1);
 		return ComponentRef<TComponent>(m_components.Last().GetId());
 	}
 
-	unsigned int GetIndex(const ComponentRef<TComponent>& ref);
 	void DeleteInstance(ComponentRef<TComponent> ref);
-
-	void SortLookups();
 
 private:
 	struct ComponentLookup
@@ -71,93 +72,17 @@ private:
 	};
 
 	UnorderedVector<TComponent> m_components;
-	UnorderedVector<ComponentLookup> m_lookups;
+	ComponentTracker m_tracker;
 };
-
-template<typename TComponent>
-	requires std::is_base_of_v<Component, TComponent>
-inline unsigned int ComponentStore<TComponent>::GetIndex(const ComponentRef<TComponent>& ref)
-{
-	// binary search
-
-	int left = 0;
-	int right = m_lookups.Size() - 1;
-	ComponentLookup lookup = m_lookups.Get((left + right) / 2);
-
-	int maxIterations = log2(m_lookups.Size()) + 2;
-
-	int i = 0;
-	while (lookup.Id != ref.Id && i < maxIterations)
-	{
-		if (lookup.Id <= ref.Id)
-			left = ceil((left + right) / 2.0f);
-		else
-			right = floor((left + right) / 2.0f);
-
-		lookup = m_lookups.Get((left + right) / 2);
-
-		i++;
-	}
-
-	if (i == maxIterations)
-		return -1;
-
-	return (left + right) / 2;
-}
 
 template<typename TComponent>
 	requires std::is_base_of_v<Component, TComponent>
 inline void ComponentStore<TComponent>::DeleteInstance(ComponentRef<TComponent> ref)
 {
-	unsigned int removedLookupIndex = GetIndex(ref);
-	if (removedLookupIndex == -1)
-		throw "no such instance";
+	unsigned int componentIndex = m_tracker.MarkRemoved(ref.Id);
+	m_tracker.UpdateComponentIndex(m_components.Last().GetId(), componentIndex);
+	m_components.Remove(componentIndex);
 
-	unsigned int removedComponentIndex = m_lookups.Get(removedLookupIndex).Index;
-
-	unsigned int movedComponentLookupIndex = GetIndex(ComponentRef<TComponent>(m_components.Last().GetId()));
-
-	if (movedComponentLookupIndex == -1)
-		throw "no such instance 2";
-
-	m_lookups.Set(movedComponentLookupIndex, ComponentLookup(m_components.Last().GetId(), removedComponentIndex));
-	m_lookups.Remove(removedLookupIndex);
-	m_components.Remove(removedComponentIndex);
-
-	SortLookups();
-}
-
-template<typename TComponent>
-	requires std::is_base_of_v<Component, TComponent>
-inline void ComponentStore<TComponent>::SortLookups()
-{
-	int layerSize = 1;
-
-	int totalValues = m_lookups.Size();
-
-	while (layerSize < totalValues)
-	{
-		int pointer = 0;
-		while (pointer + layerSize < totalValues)
-		{
-			int leftPointer = pointer;
-			int rightPointer = pointer + layerSize;
-
-			int valuesLeft = std::min(layerSize * 2, totalValues - pointer);
-			int lastIndex = pointer + valuesLeft - 1;
-
-			while (rightPointer <= lastIndex && leftPointer <= lastIndex)
-			{
-				if (m_lookups.Get(leftPointer).Id >= m_lookups.Get(rightPointer).Id)
-				{
-					m_lookups.Swap(leftPointer, rightPointer);
-					rightPointer++;
-				}
-
-				leftPointer++;
-			}
-			pointer += layerSize * 2;
-		}
-		layerSize *= 2;
-	}
+	if (m_tracker.EmptySlots.size() > 100)
+		m_tracker.Flush();
 }
